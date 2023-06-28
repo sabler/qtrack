2307b3d205e7938a34ba83e7ef6c153133d9f4cf
sabler <gizmo.00bases@icloud.com>
2023-06-27 21:16:27 -0700

    Hey, a map loaded! And it's in a react container!

--- src/app/dashboard/client-components/MapContainer.js
+++ src/app/dashboard/client-components/MapContainer.js
@@ -1,5 +1,32 @@
 'use client';
+// Client-side component!
+import { useEffect, useRef, useState } from 'react';
+import mapboxgl from 'mapbox-gl';
+import 'mapbox-gl/dist/mapbox-gl.css';
 
+
+// DEV TOKEN
+mapboxgl.accessToken = process.env.NEXT_PUBLIC_MAP
 export default function MapContainer() {
-  return <p>The map will be somewhere in here</p>;
+  const mapContainer = useRef(null);
+  const map = useRef(null);
+  const [lng, setLng] = useState(-70.9);
+  const [lat, setLat] = useState(42.35);
+  const [zoom, setZoom] = useState(9);
+
+  useEffect(() => {
+    if (map.current) return; // initialize map only once
+    map.current = new mapboxgl.Map({
+      container: mapContainer.current,
+      style: 'mapbox://styles/mapbox/streets-v12',
+      center: [lng, lat],
+      zoom: zoom
+    });
+  });
+
+  return (
+    <div>
+      <div ref={mapContainer} className="map-container" />
+    </div>
+  )
 }
