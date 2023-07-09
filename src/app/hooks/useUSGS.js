0006ccf976613a102ec1e68f0e66ddd78fba1fda
sabler <gizmo.00bases@icloud.com>
2023-07-08 19:18:47 -0700

    Smaller feed for now. It's fine. Everything's fine.

--- src/app/hooks/useUSGS.js
+++ src/app/hooks/useUSGS.js
@@ -3,7 +3,7 @@ import useSWR from 'swr';
 export default function useUSGS() {
   const fetcher = (...args) => fetch(...args).then((res) => res.json());
   const { data, error, isLoading } = useSWR(
-    `https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/4.5_day.geojson`,
+    `https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_hour.geojson`,
     fetcher
   );
 
