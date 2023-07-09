eb772d86a335063ad69e20654b929763df718207
sabler <gizmo.00bases@icloud.com>
2023-07-09 15:58:39 -0700

    This table is more trouble than it's worth

--- src/app/dashboard/server-components/QuakeTable.js
+++ src/app/dashboard/server-components/QuakeTable.js
@@ -27,20 +27,20 @@ export default function QuakeTable({ qdata, updateMap }) {
         key={quake.id}
         className={`${stripeClass}cursor-pointer hover:bg-sky-500 hover:text-zinc-800`}
       >
-        <th scope='row' className='text-left px-8 py-2'>
+        <th scope='row' className='text-left px-8'>
           {quake.properties.mag}
         </th>
-        <td className='px-8 py-2'>{quake.properties.place}</td>
-        <td className='px-8 py-2'>
+        <td className='px-8'>{quake.properties.place}</td>
+        <td className='px-8'>
           {quake.geometry.coordinates[0]}, {quake.geometry.coordinates[1]}
         </td>
-        <td className='px-8 py-2'>{date}</td>
+        <td className='px-8'>{date}</td>
       </tr>
     );
   });
   console.log(qdata);
   return (
-    <div className='basis-2/3 m-8 h-96 overflow-y-auto'>
+    <div className='basis-2/3 h-96 overflow-y-auto'>
       <table className='w-full h-96 border-gray-600 table-auto text-left'>
         <thead className='rounded-t-lg bg-slate-700 sticky top-0 uppercase text-xs text-sky-200'>
           <tr>
