61801c22bc23375ca9d8299457f826770eeb6872
sabler <gizmo.00bases@icloud.com>
2023-07-08 19:16:40 -0700

    Cleaning up

--- src/app/about/page.js
+++
@@ -1,5 +0,0 @@
-import Image from 'next/image';
-
-export default function About() {
-  return <p>Lorem About</p>;
-}
--- src/app/dashboard/page.js
+++ src/app/dashboard/page.js
@@ -4,7 +4,9 @@ import QuakeTable from './server-components/QuakeTable';
 export default function Dashboard() {
   return (
     <section>
-      <MapContainer />;
+      <div className='container my-16 max-w-full'>
+        <MapContainer />
+      </div>
     </section>
   );
 }
